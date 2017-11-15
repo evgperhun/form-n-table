b3972105b6ad145daf39815b520b2fdca68310cc
Eugene Perhun <eperhun@gmail.com>
2017-11-15 14:11:17 +0000

    q

---
+++ js/form.js
@@ -0,0 +1,26 @@
+$('form input[type="submit"]').click(sendForm);
+
+function sendForm(e) {
+    e.preventDefault();
+$.ajax({
+    url: "https://formspree.io/eperhun@gmail.com", 
+    method: "POST",
+    data: {
+        ('#client-name').val(),
+        ('#client-last-name').val(),
+        ('#client-email').val(),
+        ('#client-city').val(),
+        ('#client-phone').val(),
+        ('#client-birth').val(),
+        ('#client-passport-sn').val()
+        },
+        dataType: "json"
+    })
+    .done(function(){
+        $('form')[0].reset();
+        $('form').html('<h2>Дякую!</h2>');
+    })
+    .fail(function(){
+        $('#msg').html('Вибачте це помилка');
+    });
+}
